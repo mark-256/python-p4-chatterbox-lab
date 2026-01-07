9c7f4f3e6df2df0dbd035bf5b3cfd88e85838430
Mark <markjohnguto@gmail.com>
2026-01-07 12:34:00 +0300

    changes

--- server/app.py
+++ server/app.py
@@ -16,11 +16,40 @@ db.init_app(app)
 
 @app.route('/messages')
 def messages():
-    return ''
+    messages = Message.query.order_by(Message.created_at.asc()).all()
+    return jsonify([message.to_dict() for message in messages])
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+    message = Message(
+        body=data['body'],
+        username=data['username']
+    )
+    db.session.add(message)
+    db.session.commit()
+    return jsonify(message.to_dict()), 201
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    message = Message.query.filter_by(id=id).first()
+    if not message:
+        return jsonify({'error': 'Message not found'}), 404
+    
+    data = request.get_json()
+    message.body = data.get('body', message.body)
+    db.session.commit()
+    return jsonify(message.to_dict())
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = Message.query.filter_by(id=id).first()
+    if not message:
+        return jsonify({'error': 'Message not found'}), 404
+    
+    db.session.delete(message)
+    db.session.commit()
+    return '', 204
 
 if __name__ == '__main__':
     app.run(port=5555)
